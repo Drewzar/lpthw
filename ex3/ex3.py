c49af4dfa6e306442aff0ef4025b09dd8c1f92a1
Drew Oliner <drew@hackinggibsons.com>
2011-03-19 17:23:43 -0400

    I'm drew and I'm a loser

---
+++ ex3/ex3.py
@@ -0,0 +1,35 @@
+#Exercise 3: Numbers and Math
+
+#Prints what I'm doing.
+print "I will now count my chickens:"
+
+#Counts Hens and Roosters by doing math!
+print "Hens", 25.0 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+#What I'm doing next
+print "I will now count the eggs:"
+
+#printing the outcome of this math problem
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Printing a line that's asking a questing
+print "Is it ture that 3 + 2 < 5 - 7?"
+
+#Answering the question
+print 3 + 2 < 5 - 7
+
+#Breaking the question down to understand the answer
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#More line printing
+print "Oh, that's why it's False."
+
+#More line printing 
+print "How about some more."
+
+#Testing each math symbol
+print "Is it greater?", 5 > -2 
+print "Is it greater or equal?", 5 >= -2 
+print "Is it less or equal?", 5 <= -2 
---
+++ ex5/ex5ec1.py
@@ -0,0 +1,19 @@
+# Exercise 5: More Variable ans Printing
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inces tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffe." % teeth
+
+# This line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
