83f614d4fc0373b50e141bacccbd2f2bbcaf32f8
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 03:23:48 -0400

    deleted a file?

--- ex11/ex11ec2.py
+++
@@ -1,8 +0,0 @@
-# Excercise 11: Extra credit #3
-
-print "Welcome to the inch to centimeter converter."
-inch = input("Enter the number of inches: ")
-CM = 2.54
- 
-print "%r inches in centimeters is %f " % (inch, inch * CM) 
-
