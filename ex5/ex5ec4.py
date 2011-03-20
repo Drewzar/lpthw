249889c45f4784f2494374b4cdf3a3256616205b
Drew Oliner <drew@hackinggibsons.com>
2011-03-19 21:49:23 -0400

    fix my math

--- ex5/ex5ec4.py
+++ ex5/ex5ec4.py
@@ -1,11 +1,11 @@
 # Exercise 5 Extra Credit 4
 
-x = 25
-i = .39
+i = 25
+c = 2.54
 
 p = 40
 k = 2.2
 
-print "25 inches in centimeters is %f" % (x * i)
+print "25 inches in centimeters is %f" % (i * c)
 print "40 pounds in kilograms is %f" % (p / k)
 
