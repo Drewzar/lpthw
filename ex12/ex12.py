87d64c3a348c7d63f5fd9db251f9866b7aa52b59
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 03:22:09 -0400

    exercise 12

---
+++ ex12/ex12.py
@@ -0,0 +1,8 @@
+#Exercise 12: Prompting People
+
+age = raw_input("How old are? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("how much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
