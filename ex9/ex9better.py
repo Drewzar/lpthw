e41b622be20cfba7917e2a742b4d4412e6440fd2
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 00:51:15 -0400

    exercise 9

---
+++ ex9/ex9better.py
@@ -0,0 +1,17 @@
+# Exercise 9: Printing, Printing, Printing
+
+#Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thru Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+# added \n to make it look better ;D
+print "Here are the months: \n", months
+
+print """
+There's something going on here.
+With the three double-quotes
+We'll be able to type as much as we like.
+"""
+
