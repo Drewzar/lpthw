39f3f293dfe4cabb348eb5c84ffab9a37d714c64
Drew Oliner <drew@hackinggibsons.com>
2011-03-19 23:40:51 -0400

    exercise 6

---
+++ ex6/ex6.py
@@ -0,0 +1,22 @@
+# Exercise 6: Strings and Text
+
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funn?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a strong with a right side."
+
+print w + e
