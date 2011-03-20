37c5f2c0d8eb87e200baf13163bf4502e9373fd8
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 00:13:24 -0400

    spell checking

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -12,7 +12,7 @@ print "I said: %r." % x
 print "I also said: '%s'." % y
 
 hilarious = False
-joke_evaluation = "Isn't that joke so funn?! %r"
+joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
