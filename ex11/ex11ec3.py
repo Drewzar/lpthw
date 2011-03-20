22ec2d3e3963ea791ec6c02156b618a200df05f5
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 03:07:46 -0400

    Fixed extra credit number in file name

---
+++ ex11/ex11ec3.py
@@ -0,0 +1,8 @@
+# Excercise 11: Extra credit #3
+
+print "Welcome to the inch to centimeter converter."
+inch = input("Enter the number of inches: ")
+CM = 2.54
+ 
+print "%r inches in centimeters is %f " % (inch, inch * CM) 
+
