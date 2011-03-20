d6f1f5dce5840a17a4a1461b966995825a4d9735
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 04:02:54 -0400

    exercise 13 and extra credit

---
+++ ex13/ex13ec3.py
@@ -0,0 +1,12 @@
+# Exercise 13: Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second = argv
+
+third = raw_input("third?")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
