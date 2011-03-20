0b852f9dc90c72c6ee431920ed8851379b608af1
Drew Oliner <drew@hackinggibsons.com>
2011-03-20 03:10:27 -0400

    Fix formatting and grammer

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -2,10 +2,10 @@
 
 print "How old are you?",
 age = raw_input()
-print "How Tall are you?", 
+print "How tall are you?", 
 height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (
+print "So, you're %r old, %s tall and %r heavy." % (
     age, height, weight)
